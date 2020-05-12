e44b5999ab64528737ac7ebe9bc484237780827e
Sebosun <sebos@protonmail.com>
2020-05-12 14:13:35 +0200

    New query selectors, which together with new divs, will be used to update the display

--- calculator.js
+++ calculator.js
@@ -14,7 +14,12 @@ function multiply(a, b){
 // and fix it
 function divide(a, b){
     if (a === 0 || b === 0){
+
+        currentCal.style.color = 'red';
+        //calculator.firstElementChild.style.color = 'red';
+
         return 'ERROR'
+        
     }
     return (a/b);
     
@@ -40,14 +45,25 @@ function operate(a, b, operator){
     }
 }
 
-// Event listener, once the numbers are clicked, they are added to the entireOperation / partialOperation
+// Event listener, once the numbers are clicked, they are added to the displayCurrentCalc / partialOperation
 // variable. 
 function numbersListener(){
     numbers.forEach((button) => {
         button.addEventListener('click', (e)=>{
-            entireOperation += event.target.id;
+            // This is so that if user divides by zero and the color changes to red
+            // the color goes back after first new calculation
+
+            currentCal.style.color = 'black';
+            // calculator.firstElementChild.style.color = 'black';
+
+            displayCurrentCalc += event.target.id;
             partialOperation += event.target.id;
-            calculator.firstElementChild.textContent = entireOperation;
+            displayEntireCalc += event.target.id;
+
+            entireCalc.textContent = displayEntireCalc;
+            currentCal.textContent = displayCurrentCalc;
+            //calculator.firstElementChild.textContent = displayCurrentCalc;
+            
         })
     });
 }
@@ -60,12 +76,12 @@ function operatorListener(){
         operator.addEventListener('click', (e) =>{
             console.log(event.target.id);
             // converts number entered into float and adds it to the numbers array
-
+            
             numbersArray.push(parseFloat(partialOperation));
             partialOperation = '';
 
-
-            numbersArray = numbersArray.filter((number) =>{
+            //makes sure no number is NaN
+            numbersArray = numbersArray.filter((number) =>{ 
                 return isNaN(number) === false;
             });
 
@@ -74,35 +90,65 @@ function operatorListener(){
             if (numbersArray.length > 0){
                     if (numbersArray.length === 2){
                         if (event.target.id === '='){
+                            
                             numbersArray[0] = operate(numbersArray[0], numbersArray[1], operatorInput);
                             numbersArray.splice(1, 1)
-                            calculator.firstElementChild.textContent = numbersArray[0];
-                            entireOperation = [];
+                            
+                            entireCalc.textContent = displayEntireCalc;
+                            currentCal.textContent = numbersArray[0];
+                            //calculator.firstElementChild.textContent = numbersArray[0];
+                            
+                            
+                            displayEntireCalc = '';
+                            displayCurrentCalc = [];
                             numbersArray = [];
                             operatorInput = '';
                         }
                         else{
                             numbersArray[0] = operate(numbersArray[0], numbersArray[1], operatorInput);
                             numbersArray.splice(1, 1)
-                            entireOperation = numbersArray[0];
-                            calculator.firstElementChild.textContent = entireOperation;
+                            displayCurrentCalc = numbersArray[0];
+                        
+
+                            entireCalc.textContent = displayEntireCalc;
+                            currentCal.textContent = displayCurrentCalc;
+                            //calculator.firstElementChild.textContent = displayCurrentCalc;
                         }
                     }
                     if (['+', '-', '*', '/'].includes(event.target.id)){
                         operatorInput = event.target.id;
-                        entireOperation = numbersArray[0] + operatorInput;
+
+                        // if last element of the displayEntireCalc var is an operator
+                        // remove it, so that we may right after append a new operator
+                        if (['+', '-', '*', '/'].includes(displayCurrentCalc[displayCurrentCalc.length-1])){
+                            displayEntireCalc = displayEntireCalc.slice(0,-1);
+                        }
+                        displayEntireCalc += operatorInput;
+                        displayCurrentCalc = numbersArray[0] + operatorInput;
+                        
+
                         console.log(numbersArray, operatorInput);
-                        calculator.firstElementChild.textContent = entireOperation;
+
+                        entireCalc.textContent = displayEntireCalc;
+                        currentCal.textContent = displayCurrentCalc;
+                        // calculator.firstElementChild.textContent = displayCurrentCalc;
+
                     }
                     if (event.target.id === 'clear'){
-                        entireOperation = '';
+                        displayEntireCalc = '';
+                        displayCurrentCalc = '';
                         numbersArray = [];
                         operatorInput = '';
-                        calculator.firstElementChild.textContent = entireOperation;
+
+                        entireCalc.textContent = displayEntireCalc;
+                        currentCal.textContent = displayCurrentCalc;
+                        //calculator.firstElementChild.textContent = displayCurrentCalc;
                     }
                 }
             else{
-                calculator.firstElementChild.textContent = '';
+                entireCalc.textContent = displayEntireCalc;
+                currentCal.textContent = '';
+                //calculator.firstElementChild.textContent = '';
             }
         });
     });
@@ -111,20 +157,21 @@ function operatorListener(){
 const calculator = document.querySelector('.calculator');
 const numbers = document.querySelectorAll('.numbers');
 const operators = document.querySelectorAll('.operators');
+const entireCalc = document.querySelector('#wholeCal');
+const currentCal = document.querySelector('#currentCal')
 
 let numbersArray = [];
+
 let operatorInput = '';
 let firstNumber = '';
 let secondNumber = '';
 let partialOperation = '';
-let entireOperation = '';
+let displayCurrentCalc = '';
+let displayEntireCalc = '';
 
 
 // listens to operator and returns it when clicked, later the logic will be developed what happens
 // once the operator is clicked, maybe use cases?
-
-
-
 
 numbersListener(); // this will be have to be used in some
 operatorListener();
